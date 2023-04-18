af65244da77f6d5e1a8d6c975e7042406d458baa
hadarsnorlax <hadar.mirsky1@gmail.com>
2023-04-18 22:33:46 +0300

    Fix error with start up the server

--- src/database/books-db.js
+++ src/database/books-db.js
@@ -5,12 +5,11 @@ const bookSchema = require('src/api/models/book-schema');
 class BooksMongo extends MongoInterface {
   constructor() {
     super(config('mongo:url'), config('mongo:dbName'));
-    this.books = super.getCollection('books');
   }
 
   async getBook(id) {
     try {
-      const book = await this.books.findOne({ _id: id });
+      const book = await super.getCollection('books').findOne({ _id: id });
       return book;
     } catch (err) {
       throw Error(`Failed to get book ${id} from DB, error: ${err}`);
@@ -24,7 +23,7 @@ class BooksMongo extends MongoInterface {
     }
 
     try {
-      await this.books.insertOne(book);
+      await super.getCollection('books').insertOne(book);
     } catch (err) {
       throw Error(`Failed to insert book to DB, error: ${err}`);
     }
--- src/database/users-db.js
+++ src/database/users-db.js
@@ -5,12 +5,11 @@ const userSchema = require('src/api/models/user-schema');
 class UsersMongo extends MongoInterface {
   constructor() {
     super(config('mongo:url'), config('mongo:dbName'));
-    this.users = super.getCollection('users');
   }
 
   async getAllUsers() {
     try {
-      const users = await this.users.find({}).toArray();
+      const users = await super.getCollection('users').find({}).toArray();
       return users;
     } catch (err) {
       throw Error(`Failed to get all users from DB, error: ${err}`);
@@ -24,7 +23,7 @@ class UsersMongo extends MongoInterface {
     }
 
     try {
-      await this.users.insertOne(user);
+      await super.getCollection('users').insertOne(user);
     } catch (err) {
       throw Error(`Failed to insert user to DB, error: ${err}`);
     }
